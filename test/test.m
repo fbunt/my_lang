d79b9f3e32830d6b6a2fafeaab84c9cabd2c43d2
Fred Bunt <fredrick.bunt@gmailc.om>
2020-03-23 05:52:41 -0600

    Display more features in example

--- test/test.m
+++ test/test.m
@@ -1,3 +1,4 @@
+// Add 10 to a
 fn add10(a: int) -> int {
     for i in (0 .. 10) {
         a = a + 1
@@ -5,6 +6,9 @@ fn add10(a: int) -> int {
     return a
 }
 
+/*
+This is a block comment
+*/
 fn sub5(a: double) -> double {
     i: int = 5
     while i > 0 {
@@ -14,7 +18,15 @@ fn sub5(a: double) -> double {
     return a
 }
 
+fn add25(a: int) -> int {
+    for i in (0 .. 5 * 5) {
+        a = a + 1
+    }
+    return a
+}
+
 fn main() {
     printi(add10(2))
     printd(sub5(10))
+    printi(add25(5))
 }
